bb5336e1336e02d6f9bbf7a4520ec26271232d0b
shininghyunho <gusgh3315@uos.ac.kr>
2019-11-19 21:17:46 +0900

    hyunho

--- database/schedule_schema.js
+++ database/schedule_schema.js
@@ -13,14 +13,16 @@ var Schema = {};
 
 Schema.createSchema = function(mongoose) {
 
-	// 스키마 정의
-	var UserSchema = mongoose.Schema({
-      nickname: {type: String, required: true, unique: false, 'default':''}, // google user로 받음
-	    email: {type: String, required: true, unique: false, 'default':''}, // google user로 받음
-	    club: {type: String, 'default':'none'}, // 필수는 아님
-	    rank: {type: Number, 'default': 3}, // 일반 사용자 : 3, 동아리 장 : 2, 마스터 계정 : 1
-	    created_at: {type: Date, index: {unique: false}, 'default': Date.now},
-	    updated_at: {type: Date, index: {unique: false}, 'default': Date.now}
+	// 스키마 정의 : 타이틀 설명 동아리명 시작점 끝나는점 이미지 만든날짜 수정날짜
+	var ScheduleSchema = mongoose.Schema({
+    title:{type:String,required:true,unique:false,'default':''},
+    description:{type:String,required:false,unique:false,'default':'none'},
+    club:{type:String,required:true,unique:false,'default':''},
+    start:{type:Date,required:true,unique:false,'default':''},
+    end:{type:Date,required:true,unique:false,'default':''},
+    title:{type:String,required:false,unique:false,'default':''},
+    created_at: {type: Date, index: {unique: false}, 'default': Date.now},
+    updated_at: {type: Date, index: {unique: false}, 'default': Date.now}
 	});
 
 	// password를 virtual 메소드로 정의 : MongoDB에 저장되지 않는 편리한 속성임. 특정 속성을 지정하고 set, get 메소드를 정의함
@@ -35,18 +37,14 @@ Schema.createSchema = function(mongoose) {
 	})
 	*/
 
-	// 스키마에 static 메소드 추가
-	UserSchema.static('findByEmail', function(email, callback) {
-		return this.find({email:email}, callback);
-	});
 
-	UserSchema.static('findAll', function(callback) {
+	ScheduleSchema.static('findAll', function(callback) {
 		return this.find({}, callback);
 	});
 
-	console.log('UserSchema 정의함.');
+	console.log('ScheduleSchema 정의함.');
 
-	return UserSchema;
+	return ScheduleSchema;
 };
 
 // module.exports에 UserSchema 객체 직접 할당
